7c4bbf84f0030e2a001f1ef1764cf587682bd9bb
Paulina Chojecka <paula.chojecka@gmail.com>
2017-08-19 09:48:30 +0200

    java 1

---
+++ javascript/variables.js
@@ -0,0 +1,21 @@
+var imie;
+var imieDamskie = "Kaja";
+var wiek = 37;
+
+
+console.log(imie);
+console.log(imieDamskie);
+console.log(wiek);
+
+imie =  'Adam';
+imieDamskie = "Zosia";
+
+console.log("***************");
+console.log(imieDamskie);
+console.log(imie);
+
+imie = imieDamskie;
+
+console.log(imie);
+console.log(imieDamskie);
+console.log(wiek);
