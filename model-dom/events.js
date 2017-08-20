f272b294ec1fc656413d78b4da64e425af213a23
Paulina Chojecka <paula.chojecka@gmail.com>
2017-08-20 15:13:31 +0200

    start warsztatow

--- model-dom/events.js
+++ model-dom/events.js
@@ -4,5 +4,19 @@ function klikHeader() {
     console.log("Kliknąłeś w header");
 }
 
+
+function alarm(event) {
+    event.preventDefault(); //zapobiegamy domyślnej akcji
+    console.log('Kliknięto link');
+    console.log(event.type);
+}
+
+var secondLink = document.getElementsByTagName('a')[1];
+secondLink.onclick = alarm;
+
 document.getElementById('title-header').onclick = klikHeader;
 //wywołaj funkcję na elemencie o id "title-header"
+
+
+var paragraf = document.getElementsByTagName('p') [0];
+paragraf.addEventListener('click', klikHeader);
